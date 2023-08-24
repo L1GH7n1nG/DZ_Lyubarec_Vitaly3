845197d4602244b7c87ca162fef54a6e37485b23
L1GH7n1nG <l1ghtawp1@gmail.com>
2023-08-24 18:17:52 +0300

    zadacha19 added

---
+++ zadacha19/Program.cs
@@ -0,0 +1,20 @@
+﻿//Использую long и ToInt64, чтобы использовать числа более чем пятизначные.
+bool Palindrome(long num1)
+{
+	long originalNumber = num1;
+	long reversedNumber = 0;
+	while (num1 > 0)
+	{
+		long lastdigit = num1 % 10;
+		reversedNumber = reversedNumber * 10 + lastdigit;
+		num1 /= 10;
+	}
+	return originalNumber == reversedNumber;
+}
+
+Console.Write("Введите целое число: ");
+long n1 = Convert.ToInt64(Console.ReadLine());
+
+bool result = Palindrome(n1);
+Console.Write($"{n1} -> ");
+Console.WriteLine(result ? "Да" : "Нет");
