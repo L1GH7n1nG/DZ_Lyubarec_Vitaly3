70af8523ea0b7c219553853bde6c562537d09261
L1GH7n1nG <l1ghtawp1@gmail.com>
2023-08-24 18:18:07 +0300

    zadacha23 added

---
+++ zadacha23/Program.cs
@@ -0,0 +1,15 @@
+﻿void square(int num1)
+{
+	int i = 1;
+	Console.WriteLine($"{num1} -> ");
+	while (i <= num1)
+	{
+		Console.WriteLine($"{i} | {i * i * i,6} ");
+		i++;
+	}
+}
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+square(N);
